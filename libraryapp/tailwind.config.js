1f426d08cd1737aacb96b9dfacd3ea1067756bc2
bhaskarswagato <bhaskarswagato@gmail.com>
2024-03-15 00:24:14 +0530

    Added tailwindcss with npm

---
+++ libraryapp/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/templates/*.html", "./app/templates/user_pages/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
