87ea009906a7ca0ab42e9fa6d7242efbd5f0eab8
bhaskarswagato <bhaskarswagato@gmail.com>
2024-03-15 23:20:13 +0530

    CSS changes

--- libraryapp/app/routes/main_routes.py
+++ libraryapp/app/routes/main_routes.py
@@ -6,7 +6,8 @@ from ..models import User, Subscription
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/")
-def hello():
+@main_bp.route("/home")
+def home():
     return render_template('home.html')
 
 @main_bp.route('/users')
